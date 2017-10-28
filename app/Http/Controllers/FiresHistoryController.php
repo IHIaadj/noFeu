3f1bb867a28d30a0da11ef0daaff2ec71c60840f
Debbagh Nadir <nadir199@live.fr>
2017-10-28 16:08:49 +0100

    Ajout de la méthode de filtrage (recherche) des feux pour la route ajax

--- app/Http/Controllers/FiresHistoryController.php
+++ app/Http/Controllers/FiresHistoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Capteur;
 use App\Fire;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,57 @@ class FiresHistoryController extends Controller
         $NUMBER_OF_FIRES_PER_PAGE=15;
         $fires=Fire::paginate($NUMBER_OF_FIRES_PER_PAGE);
 
-        return view('fires.fires-history', ['fires' => $fires]);
+        //Régions
+        $regions=Capteur::select("REGION")->groupBy("REGION")->get();
+
+        return view('fires.fires-history', ['fires' => $fires,'regions'=>$regions]);
+    }
+
+    public function ajaxFireFilter(Request $request){
+        $type_rech=$request->get("typeRech");
+        $result=Fire::where("id",">","-1");
+        if($type_rech){
+            if(in_array("region",$type_rech)){
+                $region=$request->get("region");
+                $capteur=$request->get("capteur");
+                if($capteur!==null){
+                    $result=$result->where("ID_CAPTEUR","=",$capteur);
+                }
+                else if($region!=="0")
+                {
+                    $capteursReg=Capteur::select("id")->where("REGION","=",$region)->get();
+                    $result=$result->whereIn("ID_CAPTEUR",$capteursReg);
+                }
+            }
+            if(in_array("temp",$type_rech)){
+                $temp=$request->get("temperature");
+                $oper=$request->get("operation");
+                if($temp!==null){
+                    $result=$result->where("AVG_TEMP",$oper,$temp);
+                }
+
+            }
+            if(in_array("date",$type_rech)){
+                $oper=$request->get("operation-date");
+                $begTime=$request->get("beg-time");
+                if($begTime)
+                    switch($oper){
+                        case "avant":
+                            $result=$result->where("STARTING","<",$begTime);
+                            break;
+                        case "apres":
+                            $result=$result->where("STARTING",">",$begTime);
+                            break;
+                        case "entre":
+                            $endTime=$request->get("end-time");
+                            if($endTime){
+                                $result=$result->where("STARTING",">",$begTime)->where("ENDING","<",$endTime);
+                            }
+                            break;
+                    }
+            }
+        }
+        $result=$result->get();
+        dd($result);
     }
 }
