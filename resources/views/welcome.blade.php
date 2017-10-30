418b69766b5332b5972796baefbb9aa3af0c9335
Chemsou Harouit <fc_harouit@esi.dz>
2017-10-30 21:59:20 +0100

    Adding the HeatMap Reorganizing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,55 +40,40 @@
 <body>
 
 <div id="map" style="float: left;"></div>
-<div id="voletMenu"style="float: right;">Volet Menu</div>
+<div id="voletMenu" style="float: right;">Volet Menu</div>
 <div id="floating-panel">
+    <button onclick="toggleHeatmap()">Toggle Heatmap</button>
     <button onclick="toggleSmokeMarkers()">Toggle Smoke</button>
     <button onclick="toggleTemperatureMarkers()">Toggle Temperature</button>
     <button onclick="toggleHumidityMarkers()">Toggle Humidity</button>
     <button onclick="enableMarkers()">Markers</button>
 </div>
 
-<script>
+<script title="Initialize">
 
     var map;
+    var heatmap;
     var markersTable=[];
-    /**
-     * Initialize the map according to Google Map API JavaScript Models
-     * This function is called once data is loaded at the beginning according to the callback specification
-     */
-    function initMap() {
 
-        var mapCenter= new google.maps.LatLng(34.946,3.244);
+    //table issue de la BDD où chaque ligne représente un capteur et son état actuel
+    var capteursTable = {!! json_encode($Capteurs_Join_Events_id) !!};
 
-        //création de la map
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 7,
-            center: mapCenter,
-            mapTypeId: 'terrain'
-        });
-
-        //table issue de la BDD où chaque ligne représente un capteur et son état actuel
-        var capteursTable = {!! json_encode($Capteurs_Join_Events_id) !!};
-
-        //pour chaque ligne du capteur on ajoute un marker pour notre map
-        capteursTable.forEach(function (capteur) {
-            //création d'un capteur
-            createMarker(capteur);
-        });
-
+    function initialize()
+    {
+        createMap();
+        createMarkers();
+        createHeatMap();
     }
 
 
 
-
-
 </script>
 
-<script async defer title="Google Map API"
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3R7bzKOXmqJWdiKgbhtfa_DMQQ3PL1Oo&libraries=visualization&callback=initMap">
+<script async defer title="Google_Map_API"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3R7bzKOXmqJWdiKgbhtfa_DMQQ3PL1Oo&libraries=visualization&callback=initialize">
 </script>
 
-<script title="Marker Functions">
+<script title="Marker_Functions">
     /**
      * Toggle the Humidity Markers
      */
@@ -135,6 +120,13 @@
     }
 
     /**
+     * Toggle HeatMap : a map showing temperature in colors (red,yellow,green) using the Visualization Library.
+     */
+    function toggleHeatmap() {
+        heatmap.setMap(heatmap.getMap() ? null : map);
+    }
+
+    /**
      * Create a marker representing the variable "capteur" and add it to the markersTable (global variable)
      * @param capteur représente un capteur avec tous ses états actuels
      */
@@ -169,5 +161,47 @@
 
 </script>
 
+<script title="Map&Markers_Funcitons">
+
+    /**
+     * Initialize the map according to Google Map API JavaScript Models
+     * This function is called once data is loaded at the beginning according to the callback specification
+     */
+    function createMap()
+    {
+        var mapCenter= new google.maps.LatLng(34.946,3.244);
+
+        //création de la map
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 7,
+            center: mapCenter,
+            mapTypeId: 'terrain'
+        });
+    }
+
+    /**
+     * Creating the markers which will be shown on a map
+     */
+    function createMarkers()
+    {
+        //pour chaque ligne du capteur on ajoute un marker pour notre map
+        capteursTable.forEach(function (capteur) {
+            //création d'un capteur
+            createMarker(capteur);
+        });
+    }
+
+    /**
+     * Creating the HeatMap from a markers Table to show temperature in colors Based on Visualization Library
+     * Should be called after creating markers
+     */
+    function createHeatMap()
+    {
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: markersTable.map(function(value,index) { return value['position']; }),
+            map: map
+        });
+    }
+</script>
 </body>
 </html>
